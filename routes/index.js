4e469f323f67b9b9471fffd1be9fe055bd2ee53c
SethKaminski <sethgabe@yahoo.com>
2017-02-10 16:44:29 -0500

    adding bootstrap

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,20 @@ router.get('/about', (req, res, next) => {
   res.render('about', { title: 'About' });
 });
 
+/* GET Projects page. */
+router.get('/projects', (req, res, next) => {
+  res.render('index', { title: 'Projects' });
+});
+
+/* GET Services page. */
+router.get('/services', (req, res, next) => {
+  res.render('index', { title: 'Services' });
+});
+
+/* GET Contact page. */
+router.get('/contact', (req, res, next) => {
+  res.render('index', { title: 'Contact' });
+});
+
+
 module.exports = router;
