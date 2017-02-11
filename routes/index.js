8ba291571393731c6b55681c3067012556b15fd2
SethKaminski <sethgabe@yahoo.com>
2017-02-10 20:28:21 -0500

    Added placeholder content and var to lets

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,27 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Home' });
+  res.render('content/index', { title: 'Home' });
 });
 
 /* GET about page. */
 router.get('/about', (req, res, next) => {
-  res.render('about', { title: 'About' });
+  res.render('content/about', { title: 'About' });
 });
 
 /* GET Projects page. */
 router.get('/projects', (req, res, next) => {
-  res.render('projects', { title: 'Projects' });
+  res.render('content/projects', { title: 'Projects' });
 });
 
 /* GET Services page. */
 router.get('/services', (req, res, next) => {
-  res.render('services', { title: 'Services' });
+  res.render('content/services', { title: 'Services' });
 });
 
 /* GET Contact page. */
 router.get('/contact', (req, res, next) => {
-  res.render('contact', { title: 'Contact' });
+  res.render('content/contact', { title: 'Contact' });
 });
 
 
